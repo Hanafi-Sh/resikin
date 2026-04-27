97aa6e88d03c32af86fe52840f27b7ea996659c8
Han <hanafishiddiq2@gmail.com>
2026-04-27 11:59:18 +0700

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@huggingface/transformers', 'onnxruntime-node'],
 };
 
 export default nextConfig;
